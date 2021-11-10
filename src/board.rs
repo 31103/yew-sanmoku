99cc4491bc9b4a0839d40efd58465b0892b39f9b
31103 <swpreit@gmail.com>
2021-11-10 21:15:29 +0900

    liftup state

--- src/board.rs
+++ src/board.rs
@@ -2,10 +2,15 @@ use yew::prelude::*;
 
 use crate::square::Square;
 
+#[derive(Clone, PartialEq, Properties)]
+pub struct Props {
+    pub squares: Vec<&'static str>,
+    pub on_click: Callback<usize>,
+}
+
 pub struct Board {
+    props: Props,
     link: ComponentLink<Self>,
-    squares: Vec<&'static str>,
-    x_is_next: bool,
 }
 
 pub enum Msg {
@@ -14,47 +19,33 @@ pub enum Msg {
 
 impl Component for Board {
     type Message = Msg;
-    type Properties = ();
+    type Properties = Props;
 
-    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Board {
-            link,
-            squares: vec![""; 9],
-            x_is_next: true,
-        }
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self { props, link }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::Click(i) => {
-                if self.calculate_winner().is_some() || !self.squares[i].is_empty() {
-                    return false;
-                }
-                self.squares[i] = if self.x_is_next { "X" } else { "O" };
-                self.x_is_next = !self.x_is_next;
+                self.props.on_click.emit(i);
                 true
             }
         }
     }
 
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
-        false
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        if props != self.props {
+            self.props = props;
+            true
+        } else {
+            false
+        }
     }
 
     fn view(&self) -> Html {
-        // let status = format!("Next player: {}", if self.x_is_next { "X" } else { "O" });
-        let status;
-        match self.calculate_winner() {
-            Some(w) => status = format!("Winner: {}", w),
-            None => status = format!("Next player: {}", if self.x_is_next { "X" } else { "O" }),
-        }
-
         html! {
             <div>
-                <div class="status">{ status }</div>
                 <div class="board-row">
                     {self.render_square(0)}
                     {self.render_square(1)}
@@ -79,30 +70,9 @@ impl Board {
     fn render_square(&self, i: usize) -> Html {
         html! {
             <Square
-                value=self.squares[i]
+                value=self.props.squares[i]
                 on_click=self.link.callback(move |_| Msg::Click(i))
             />
         }
-    }
-
-    fn calculate_winner(&self) -> Option<&'static str> {
-        let squares = &self.squares;
-        let lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        for i in 0..lines.len() {
-            let [a, b, c] = lines[i];
-            if !squares[a].is_empty() && squares[a] == squares[b] && squares[a] == squares[c] {
-                return Some(squares[a]);
-            }
-        }
-        None
     }
 }
--- src/game.rs
+++ src/game.rs
@@ -3,22 +3,40 @@ use yew::prelude::*;
 use crate::board::Board;
 
 pub struct Game {
-    _link: ComponentLink<Self>,
+    link: ComponentLink<Self>,
+    history: Vec<Vec<&'static str>>,
+    x_is_next: bool,
 }
 
-pub enum Msg {}
+pub enum Msg {
+    Click(usize),
+}
 
 impl Component for Game {
     type Message = Msg;
     type Properties = ();
-    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Self { _link }
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            history: vec![vec![""; 9]],
+            x_is_next: true,
+        }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        // match msg {
-        // }
-        true
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Click(i) => {
+                let history = &self.history;
+                let mut current = history.iter().last().unwrap().clone();
+                if calculate_winner(&current).is_some() || !current[i].is_empty() {
+                    return false;
+                }
+                current[i] = if self.x_is_next { "X" } else { "O" };
+                self.history.push(current);
+                self.x_is_next = !self.x_is_next;
+                true
+            }
+        }
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
@@ -29,18 +47,44 @@ impl Component for Game {
     }
 
     fn view(&self) -> Html {
+        let history = &self.history;
+        let current = history.iter().last().unwrap();
+        let status;
+        match calculate_winner(current) {
+            Some(w) => status = format!("Winner: {}", w),
+            None => status = format!("Next player: {}", if self.x_is_next { "X" } else { "O" }),
+        }
+
         html! {
-            <>
-                <div class="game">
-                    <div class="game-board">
-                        <Board />
-                    </div>
+            <div class="game">
+                <div class="game-board">
+                    <Board squares=current.clone() on_click=self.link.callback(move |i:usize|Msg::Click(i)) />
                 </div>
                 <div class="game-info">
-                    <div>/* status*/</div>
+                    <div>{ status }</div>
                     <ol>/* TODO */</ol>
                 </div>
-            </>
+            </div>
         }
     }
+}
+
+fn calculate_winner(squares: &Vec<&'static str>) -> Option<&'static str> {
+    let lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for i in 0..lines.len() {
+        let [a, b, c] = lines[i];
+        if !squares[a].is_empty() && squares[a] == squares[b] && squares[a] == squares[c] {
+            return Some(squares[a]);
+        }
+    }
+    None
 }
