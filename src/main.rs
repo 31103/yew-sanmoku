d977627a573437c21cbe4a99d3489c749cb38bfe
31103 <swpreit@gmail.com>
2021-11-15 21:43:08 +0900

    file size adjustiment

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ mod game;
 mod info;
 mod square;
 
+extern crate wee_alloc;
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 pub fn main() {
     yew::start_app::<game::Game>();
 }
